37ff369152a27eaa5d5961166dbcfd35a60c04a5
Pavel <topalife.world@gmail.com>
2020-11-22 14:47:23 +0100

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-//
+//strict mode
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+*/
+
+//functions
+/*
+function logger() {
+    console.log('My name is Pavel');
+}
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+//functions declaration
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// function expretion --- prefear(more nicer code)
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+//Arrow function
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Pavel'));
+console.log(yearsUntilRetirement(1980, 'Maryna'));
